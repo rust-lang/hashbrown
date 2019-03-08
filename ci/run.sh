5393b1b0250dc5096ad3c3fa63fc5fa5c03c9d4a
gnzlbg <gonzalobg88@gmail.com>
2019-03-08 10:40:34 +0100

    Upgrade CI (clippy, more targets)

---
+++ ci/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+set -ex
+
+: "${TARGET?The TARGET environment variable must be set.}"
+
+FEATURES="rayon,serde"
+if [ "${TRAVIS_RUST_VERSION}" = "nightly" ]; then
+    FEATURES="${FEATURES},nightly"
+fi
+
+CARGO=cargo
+if [ "${CROSS}" = "1" ]; then
+    export CARGO_NET_RETRY=5
+    export CARGO_NET_TIMEOUT=10
+
+    cargo install cross
+    CARGO=cross
+fi
+
+"${CARGO}" -vv test --target="${TARGET}"
+"${CARGO}" -vv test --target="${TARGET}" --features "${FEATURES}"
+
+"${CARGO}" -vv test --target="${TARGET}" --release
+"${CARGO}" -vv test --target="${TARGET}" --features "${FEATURES}"
+
+if [ "${TRAVIS_RUST_VERSION}" = "nightly" ]; then
+    # Run benchmark on native targets, build them on non-native ones:
+    NO_RUN=""
+    if [ "${CROSS}" = "1" ]; then
+        NO_RUN="--no-run"
+    fi
+
+    "${CARGO}" -vv bench "${NO_RUN}" --features "${FEATURES}"
+fi
---
+++ ci/tools.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+set -ex
+
+retry() {
+    result=0
+    count=1
+    max=5
+    while [ "$count" -le 3 ]; do
+        [ "$result" -ne 0 ] && {
+            printf "\nRetrying, %d of %d\n" $count $max >&2
+        }
+        "$@"
+        result=$?
+        [ $result -eq 0 ] && break
+        count=$(count + 1)
+        sleep 1
+    done
+
+    [ "$count" -gt 3 ] && {
+        printf "\nFailed %d times.\n" $max >&2
+    }
+
+    return $result
+}
+
+
+if retry rustup component add rustfmt ; then
+    cargo fmt --all -- --check
+fi
+
+if retry rustup component add clippy ; then
+    cargo clippy --all -- -D clippy::pedantic
+fi
+
+if command -v shellcheck ; then
+    shellcheck --version
+    shellcheck ci/*.sh
+fi
