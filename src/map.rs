31fc802d4d73a40c53a8ca5575942c6c4b0ac6e8
Amanieu d'Antras <amanieu@gmail.com>
2019-08-04 19:11:00 +0200

    Update README

--- src/map.rs
+++ src/map.rs
@@ -18,7 +18,7 @@ pub enum DefaultHashBuilder {}
 
 /// A hash map implemented with quadratic probing and SIMD lookup.
 ///
-/// The default hashing algorithm is currently `fx`, though this is
+/// The default hashing algorithm is currently [AHash], though this is
 /// subject to change at any point in the future. This hash function is very
 /// fast for all types of keys, but this algorithm will typically *not* protect
 /// against attacks such as HashDoS.
@@ -145,6 +145,7 @@ pub enum DefaultHashBuilder {}
 /// [`with_hasher`]: #method.with_hasher
 /// [`with_capacity_and_hasher`]: #method.with_capacity_and_hasher
 /// [`fnv`]: https://crates.io/crates/fnv
+/// [AHash]: https://crates.io/crates/ahash
 ///
 /// ```
 /// use hashbrown::HashMap;
