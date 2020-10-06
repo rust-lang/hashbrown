34bab11f672c30b836a70426d06cc4dbedf64831
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-10-06 10:28:24 +0200

    fix rayon feature

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -736,7 +736,6 @@ impl<T, A: Allocator + Clone> RawTable<T, A> {
                     // Hash the current item
                     let item = guard.bucket(i);
                     let hash = hasher(item.as_ref());
-
                     match guard.search_new_slot(i, hash) {
                         Slot::Skip => continue 'outer,
                         Slot::Empty(new_i) => {
@@ -1702,7 +1701,7 @@ impl<T> RawIterRange<T> {
 
                 let tail = Self::new(
                     self.inner.next_ctrl.add(mid),
-                    self.inner.data.next_n(Group::WIDTH).next_n(mid),
+                    self.data.next_n(Group::WIDTH).next_n(mid),
                     len - mid,
                 );
                 debug_assert_eq!(
