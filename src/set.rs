e269fca01aa0e71640c806e9e7bafacb166f4f79
Dirk Stolle <striezel-dev@web.de>
2021-07-02 00:01:16 +0200

    adjustment to make clippy happy
    It seems it does not like `cloned()` when `copied()` can be used.
    See <https://github.com/rust-lang/hashbrown/runs/2967101005> for
    the details.

--- src/set.rs
+++ src/set.rs
@@ -1168,7 +1168,7 @@ where
 {
     #[cfg_attr(feature = "inline-more", inline)]
     fn extend<I: IntoIterator<Item = &'a T>>(&mut self, iter: I) {
-        self.extend(iter.into_iter().cloned());
+        self.extend(iter.into_iter().copied());
     }
 
     #[inline]
