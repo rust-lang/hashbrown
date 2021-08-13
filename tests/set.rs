5509bb1f22cb2cbb4d751f01b80fc326707cf5db
Frank Steffahn <frank.steffahn@stu.uni-kiel.de>
2021-08-13 20:42:17 +0200

    Improve code readability of a test

--- tests/set.rs
+++ tests/set.rs
@@ -2,20 +2,21 @@
 
 use hashbrown::HashSet;
 use rand::{distributions::Alphanumeric, rngs::SmallRng, Rng, SeedableRng};
+use std::iter;
 
 #[test]
 fn test_hashset_insert_remove() {
     let mut m: HashSet<Vec<char>> = HashSet::new();
-    //let num: u32 = 4096;
-    //let tx: Vec<Vec<u8>> = (0..num).map(|i| (i..(16 + i)).collect()).collect();
+
     let seed: [u8; 16] = [
         130, 220, 246, 217, 111, 124, 221, 189, 190, 234, 121, 93, 67, 95, 100, 43,
     ];
 
     let rng = &mut SmallRng::from_seed(seed);
-    let tx: Vec<Vec<char>> = (0..4096)
-        .map(|_| (rng.sample_iter(&Alphanumeric).take(32).collect()))
-        .collect();
+    let tx: Vec<Vec<char>> =
+        iter::repeat_with(|| rng.sample_iter(&Alphanumeric).take(32).collect())
+            .take(4096)
+            .collect();
 
     // more readable with explicit `true` / `false`
     #[allow(clippy::bool_assert_comparison)]
