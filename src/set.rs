8d6c27836639a8538f6e4ff8a80fa9af6ef24a79
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-09-12 19:40:29 +0100

    insert_unique_unchecked operation
    Sometimes a map is constructed when it is known that all keys are
    unique (e. e. if keys are coming from another map or from a
    sorted/deduplicated iterator). In this case we can make insertion
    faster by skipping a check that a key already exists in the map.
    
    `insert_unique_unchecked` is guaranteed to be memory-safe, but does
    not guarantee anything beyond that: if inserted key is not unique,
    `HashMap` can panic, loop forever, return incorrect entry etc.
    
    Added simple benchmark. `insert_unique_unchecked` is about 30%
    faster than `insert`.  Your mileage may vary of course.
    
    Similar PR was
    [added to `indexmap` crate](https://github.com/bluss/indexmap/pull/200)
    and they asked to discuss the name of the operation with `hashbrown`
    crate owners to come to the same naming convention (if `hashbrown`
    is willing to have the same operation).

--- src/set.rs
+++ src/set.rs
@@ -991,6 +991,28 @@ where
         self.map.insert(value, ()).is_none()
     }
 
+    /// Insert a value the set without checking if the value already exists in the set.
+    ///
+    /// This operation is safe if a value does not exist in the set.
+    ///
+    /// However, if a value exists in the set already, the behavior is unspecified:
+    /// this operation may panic, loop forever, or any following operation with the set
+    /// may panic, loop forever or return arbitrary result.
+    ///
+    /// That said, this operation (and following operations) are guaranteed to
+    /// not violate memory safety.
+    ///
+    /// This operation is faster than regular insert, because it does not perform
+    /// lookup before insertion.
+    ///
+    /// This operation is useful during initial population of the set.
+    /// For example, when constructing a set from another set, we know
+    /// that values are unique.
+    #[cfg_attr(feature = "inline-more", inline)]
+    pub fn insert_unique_unchecked(&mut self, value: T) {
+        self.map.insert_unique_unchecked(value, ());
+    }
+
     /// Adds a value to the set, replacing the existing value, if any, that is equal to the given
     /// one. Returns the replaced value.
     ///
