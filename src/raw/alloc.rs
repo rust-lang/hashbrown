05ee33e9f0fa14bc869d120bcb8e2812d39574e3
Andrew Tribick <ajtribick@googlemail.com>
2021-12-13 18:34:28 +0100

    Disable clippy::missing_safety_doc lint on Allocator trait

--- src/raw/alloc.rs
+++ src/raw/alloc.rs
@@ -33,6 +33,7 @@ mod inner {
     use crate::alloc::alloc::{alloc, dealloc, Layout};
     use core::ptr::NonNull;
 
+    #[allow(clippy::missing_safety_doc)] // not exposed outside of this crate
     pub unsafe trait Allocator {
         fn allocate(&self, layout: Layout) -> Result<NonNull<u8>, ()>;
         unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout);
