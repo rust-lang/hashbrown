cf1602a1dce2b5ff9d5ad8b80ecfaf41a6486e5c
Amanieu d'Antras <amanieu@gmail.com>
2022-05-01 17:40:41 +0100

    Fix underflow in RawIterRange::size_hint

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -1963,10 +1963,14 @@ impl<T> Iterator for RawIterRange<T> {
     #[inline]
     fn size_hint(&self) -> (usize, Option<usize>) {
         // We don't have an item count, so just guess based on the range size.
-        (
-            0,
-            Some(unsafe { offset_from(self.end, self.next_ctrl) + Group::WIDTH }),
-        )
+        let remaining_buckets = if self.end > self.next_ctrl {
+            unsafe { offset_from(self.end, self.next_ctrl) }
+        } else {
+            0
+        };
+
+        // Add a group width to include the group we are currently processing.
+        (0, Some(Group::WIDTH + remaining_buckets))
     }
 }
 
