b9e3e2e8c1e3620a5aedc67047bdf1b4bae3d90c
Zakarum <zakarumych@ya.ru>
2023-04-05 12:28:18 +0300

    At least one of "nightly" and "allocator-api2" feature is required

--- ci/run.sh
+++ ci/run.sh
@@ -27,8 +27,8 @@ if [ "${CROSS}" = "1" ]; then
 fi
 
 # Make sure we can compile without the default hasher
-"${CARGO}" -vv build --target="${TARGET}" --no-default-features
-"${CARGO}" -vv build --target="${TARGET}" --release --no-default-features
+"${CARGO}" -vv build --target="${TARGET}" --no-default-features --features=allocator-api2
+"${CARGO}" -vv build --target="${TARGET}" --release --no-default-features --features=allocator-api2
 
 "${CARGO}" -vv ${OP} --target="${TARGET}"
 "${CARGO}" -vv ${OP} --target="${TARGET}" --features "${FEATURES}"
