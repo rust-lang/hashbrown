acc5b554399970bca98af560e57602ea3c57589f
Benoît du Garreau <benoit@dugarreau.fr>
2025-09-16 16:17:17 +0200

    Restore MRSV to 1.63

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -1432,9 +1432,7 @@ impl RawTableInner {
     const fn new() -> Self {
         Self {
             // Be careful to cast the entire slice to a raw pointer.
-            ctrl: unsafe {
-                NonNull::new_unchecked(Group::static_empty().as_ptr().cast_mut().cast())
-            },
+            ctrl: unsafe { NonNull::new_unchecked(Group::static_empty().as_ptr() as *mut _) },
             bucket_mask: 0,
             items: 0,
             growth_left: 0,
