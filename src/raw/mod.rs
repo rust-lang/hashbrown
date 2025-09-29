b848171a43fcb42dd38222e4d8723953837b362a
overlookmotel <theoverlookmotel@gmail.com>
2025-09-29 12:22:21 +0100

    refactor: fix typo in comment

--- src/raw/mod.rs
+++ src/raw/mod.rs
@@ -2513,7 +2513,7 @@ impl RawTableInner {
 
     /// Gets the slice of all control bytes.
     fn ctrl_slice(&mut self) -> &mut [Tag] {
-        // SAFETY: We've intiailized all control bytes, and have the correct number.
+        // SAFETY: We've initialized all control bytes, and have the correct number.
         unsafe { slice::from_raw_parts_mut(self.ctrl.as_ptr().cast(), self.num_ctrl_bytes()) }
     }
 
