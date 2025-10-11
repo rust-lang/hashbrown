dec3142eaeb7f4ec339d7fadc7b89bb94eef3df0
Guillaume Endignoux <guillaumee@google.com>
2025-10-11 19:57:55 +0200

    Implement IntoParallelSource for hash tables.

--- src/external_trait_impls/paralight.rs
+++ src/external_trait_impls/paralight.rs
@@ -1,8 +1,8 @@
 use crate::raw::{Allocator, RawTable};
 use crate::{HashMap, HashSet};
 use paralight::iter::{
-    IntoParallelRefMutSource, IntoParallelRefSource, ParallelSource, SourceCleanup,
-    SourceDescriptor,
+    IntoParallelRefMutSource, IntoParallelRefSource, IntoParallelSource, ParallelSource,
+    SourceCleanup, SourceDescriptor,
 };
 
 // HashSet.par_iter()
@@ -203,6 +203,183 @@ impl<'data, K: Sync, V: Send + Sync, A: Allocator> SourceDescriptor
     }
 }
 
+// HashSet.into_par_iter()
+// TODO: Remove Sync requirement on T.
+impl<T: Send + Sync, S, A: Allocator + Sync> IntoParallelSource for HashSet<T, S, A> {
+    type Item = Option<T>;
+    type Source = HashSetParallelSource<T, S, A>;
+
+    fn into_par_iter(self) -> Self::Source {
+        HashSetParallelSource { hash_set: self }
+    }
+}
+
+#[must_use = "iterator adaptors are lazy"]
+pub struct HashSetParallelSource<T, S, A: Allocator> {
+    hash_set: HashSet<T, S, A>,
+}
+
+impl<T: Send + Sync, S, A: Allocator + Sync> ParallelSource for HashSetParallelSource<T, S, A> {
+    type Item = Option<T>;
+
+    fn descriptor(self) -> impl SourceDescriptor<Item = Self::Item> + Sync {
+        HashSetSourceDescriptor {
+            table: self.hash_set.map.table,
+        }
+    }
+}
+
+struct HashSetSourceDescriptor<T, A: Allocator> {
+    table: RawTable<(T, ()), A>,
+}
+
+impl<T: Send + Sync, A: Allocator> SourceCleanup for HashSetSourceDescriptor<T, A> {
+    const NEEDS_CLEANUP: bool = core::mem::needs_drop::<T>();
+
+    unsafe fn cleanup_item_range(&self, range: core::ops::Range<usize>) {
+        if Self::NEEDS_CLEANUP {
+            debug_assert!(range.start <= range.end);
+            debug_assert!(range.start <= self.len());
+            debug_assert!(range.end <= self.len());
+            for index in range {
+                // SAFETY: TODO
+                let full = unsafe { self.table.is_bucket_full(index) };
+                if full {
+                    // SAFETY: TODO
+                    let bucket = unsafe { self.table.bucket(index) };
+                    // SAFETY: TODO
+                    let (t, ()) = unsafe { bucket.read() };
+                    drop(t);
+                }
+            }
+        }
+    }
+}
+
+impl<T: Send + Sync, A: Allocator> SourceDescriptor for HashSetSourceDescriptor<T, A> {
+    type Item = Option<T>;
+
+    fn len(&self) -> usize {
+        self.table.buckets()
+    }
+
+    unsafe fn fetch_item(&self, index: usize) -> Self::Item {
+        debug_assert!(index < self.len());
+        // SAFETY: TODO
+        let full = unsafe { self.table.is_bucket_full(index) };
+        if full {
+            // SAFETY: TODO
+            let bucket = unsafe { self.table.bucket(index) };
+            // SAFETY: TODO
+            let (t, ()) = unsafe { bucket.read() };
+            Some(t)
+        } else {
+            None
+        }
+    }
+}
+
+impl<T, A: Allocator> Drop for HashSetSourceDescriptor<T, A> {
+    fn drop(&mut self) {
+        // Paralight already dropped each missing bucket via calls to cleanup_item_range(), so we
+        // can simply mark all buckets as cleared and let the RawTable destructor do the rest.
+        // TODO: Optimize this to simply deallocate without touching the control bytes.
+        self.table.clear_no_drop();
+    }
+}
+
+// HashMap.into_par_iter()
+// TODO: Remove Sync requirement on K and V.
+impl<K: Send + Sync, V: Send + Sync, S, A: Allocator + Sync> IntoParallelSource
+    for HashMap<K, V, S, A>
+{
+    type Item = Option<(K, V)>;
+    type Source = HashMapParallelSource<K, V, S, A>;
+
+    fn into_par_iter(self) -> Self::Source {
+        HashMapParallelSource { hash_map: self }
+    }
+}
+
+#[must_use = "iterator adaptors are lazy"]
+pub struct HashMapParallelSource<K, V, S, A: Allocator> {
+    hash_map: HashMap<K, V, S, A>,
+}
+
+impl<K: Send + Sync, V: Send + Sync, S, A: Allocator + Sync> ParallelSource
+    for HashMapParallelSource<K, V, S, A>
+{
+    type Item = Option<(K, V)>;
+
+    fn descriptor(self) -> impl SourceDescriptor<Item = Self::Item> + Sync {
+        HashMapSourceDescriptor {
+            table: self.hash_map.table,
+        }
+    }
+}
+
+struct HashMapSourceDescriptor<K, V, A: Allocator> {
+    table: RawTable<(K, V), A>,
+}
+
+impl<K: Send + Sync, V: Send + Sync, A: Allocator> SourceCleanup
+    for HashMapSourceDescriptor<K, V, A>
+{
+    const NEEDS_CLEANUP: bool = core::mem::needs_drop::<(K, V)>();
+
+    unsafe fn cleanup_item_range(&self, range: core::ops::Range<usize>) {
+        if Self::NEEDS_CLEANUP {
+            debug_assert!(range.start <= range.end);
+            debug_assert!(range.start <= self.len());
+            debug_assert!(range.end <= self.len());
+            for index in range {
+                // SAFETY: TODO
+                let full = unsafe { self.table.is_bucket_full(index) };
+                if full {
+                    // SAFETY: TODO
+                    let bucket = unsafe { self.table.bucket(index) };
+                    // SAFETY: TODO
+                    let key_value = unsafe { bucket.read() };
+                    drop(key_value);
+                }
+            }
+        }
+    }
+}
+
+impl<K: Send + Sync, V: Send + Sync, A: Allocator> SourceDescriptor
+    for HashMapSourceDescriptor<K, V, A>
+{
+    type Item = Option<(K, V)>;
+
+    fn len(&self) -> usize {
+        self.table.buckets()
+    }
+
+    unsafe fn fetch_item(&self, index: usize) -> Self::Item {
+        debug_assert!(index < self.len());
+        // SAFETY: TODO
+        let full = unsafe { self.table.is_bucket_full(index) };
+        if full {
+            // SAFETY: TODO
+            let bucket = unsafe { self.table.bucket(index) };
+            // SAFETY: TODO
+            unsafe { Some(bucket.read()) }
+        } else {
+            None
+        }
+    }
+}
+
+impl<K, V, A: Allocator> Drop for HashMapSourceDescriptor<K, V, A> {
+    fn drop(&mut self) {
+        // Paralight already dropped each missing bucket via calls to cleanup_item_range(), so we
+        // can simply mark all buckets as cleared and let the RawTable destructor do the rest.
+        // TODO: Optimize this to simply deallocate without touching the control bytes.
+        self.table.clear_no_drop();
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -229,6 +406,28 @@ mod test {
             .par_iter()
             .with_thread_pool(&mut thread_pool)
             .filter_map(|x| x.map(|y| y.deref()))
+            .sum::<i32>();
+        assert_eq!(sum, 21 * 43);
+    }
+
+    #[test]
+    fn test_set_into_par_iter() {
+        let mut set = HashSet::new();
+        for i in 1..=42 {
+            set.insert(Box::new(i));
+        }
+
+        let mut thread_pool = ThreadPoolBuilder {
+            num_threads: ThreadCount::AvailableParallelism,
+            range_strategy: RangeStrategy::WorkStealing,
+            cpu_pinning: CpuPinningPolicy::No,
+        }
+        .build();
+
+        let sum = set
+            .into_par_iter()
+            .with_thread_pool(&mut thread_pool)
+            .filter_map(|x| x.map(|y| *y))
             .sum::<i32>();
         assert_eq!(sum, 21 * 43);
     }
@@ -275,5 +474,25 @@ mod test {
         for (k, v) in map.iter() {
             assert_eq!(**k * **k, **v);
         }
+    }
+
+    #[test]
+    fn test_map_into_par_iter() {
+        let mut map = HashMap::new();
+        for i in 1..=42 {
+            map.insert(Box::new(i), Box::new(i * i));
+        }
+
+        let mut thread_pool = ThreadPoolBuilder {
+            num_threads: ThreadCount::AvailableParallelism,
+            range_strategy: RangeStrategy::WorkStealing,
+            cpu_pinning: CpuPinningPolicy::No,
+        }
+        .build();
+
+        map.into_par_iter()
+            .with_thread_pool(&mut thread_pool)
+            .filter_map(|x| x)
+            .for_each(|(k, v)| assert_eq!(*k * *k, *v));
     }
 }
