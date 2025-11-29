b1d85798e312eb985e0e5ade28f1a28b536ba84b
ltdk <usr@ltdk.xyz>
2025-11-29 16:25:54 -0500

    Call HashTable methods where exact matches exist

--- src/map.rs
+++ src/map.rs
@@ -483,7 +483,7 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     /// Returns a reference to the underlying allocator.
     #[inline]
     pub fn allocator(&self) -> &A {
-        self.table.raw.allocator()
+        self.table.allocator()
     }
 
     /// Creates an empty `HashMap` which will use the given hash builder to hash
@@ -587,7 +587,7 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     /// ```
     #[cfg_attr(feature = "inline-more", inline)]
     pub fn capacity(&self) -> usize {
-        self.table.raw.capacity()
+        self.table.capacity()
     }
 
     /// An iterator visiting all keys in arbitrary order.
@@ -780,7 +780,7 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     #[cfg(test)]
     #[cfg_attr(feature = "inline-more", inline)]
     fn raw_capacity(&self) -> usize {
-        self.table.raw.num_buckets()
+        self.table.num_buckets()
     }
 
     /// Returns the number of elements in the map.
@@ -797,7 +797,7 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     /// ```
     #[cfg_attr(feature = "inline-more", inline)]
     pub fn len(&self) -> usize {
-        self.table.raw.len()
+        self.table.len()
     }
 
     /// Returns `true` if the map contains no elements.
@@ -889,15 +889,8 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     where
         F: FnMut(&K, &mut V) -> bool,
     {
-        // Here we only use `iter` as a temporary, preventing use-after-free
-        unsafe {
-            for item in self.table.raw.iter() {
-                let &mut (ref key, ref mut value) = item.as_mut();
-                if !f(key, value) {
-                    self.table.raw.erase(item);
-                }
-            }
-        }
+        self.table
+            .retain(move |&mut (ref key, ref mut val)| f(key, val))
     }
 
     /// Drains elements which are true under the given predicate,
@@ -978,7 +971,7 @@ impl<K, V, S, A: Allocator> HashMap<K, V, S, A> {
     /// ```
     #[cfg_attr(feature = "inline-more", inline)]
     pub fn clear(&mut self) {
-        self.table.raw.clear();
+        self.table.clear();
     }
 
     /// Creates a consuming iterator visiting all the keys in arbitrary order.
@@ -1273,9 +1266,9 @@ where
         Q: Hash + Equivalent<K> + ?Sized,
     {
         // Avoid `Option::map` because it bloats LLVM IR.
-        if !self.table.raw.is_empty() {
+        if !self.table.is_empty() {
             let hash = make_hash::<Q, S>(&self.hash_builder, k);
-            match self.table.raw.get(hash, equivalent_key(k)) {
+            match self.table.find(hash, equivalent_key(k)) {
                 Some((_, v)) => Some(v),
                 None => None,
             }
@@ -1306,9 +1299,9 @@ where
         Q: Hash + Equivalent<K> + ?Sized,
     {
         // Avoid `Option::map` because it bloats LLVM IR.
-        if !self.table.raw.is_empty() {
+        if !self.table.is_empty() {
             let hash = make_hash::<Q, S>(&self.hash_builder, k);
-            match self.table.raw.get(hash, equivalent_key(k)) {
+            match self.table.find(hash, equivalent_key(k)) {
                 Some((key, value)) => Some((key, value)),
                 None => None,
             }
@@ -1343,9 +1336,9 @@ where
         Q: Hash + Equivalent<K> + ?Sized,
     {
         // Avoid `Option::map` because it bloats LLVM IR.
-        if !self.table.raw.is_empty() {
+        if !self.table.is_empty() {
             let hash = make_hash::<Q, S>(&self.hash_builder, k);
-            match self.table.raw.get_mut(hash, equivalent_key(k)) {
+            match self.table.find_mut(hash, equivalent_key(k)) {
                 Some(&mut (ref key, ref mut value)) => Some((key, value)),
                 None => None,
             }
@@ -1375,9 +1368,9 @@ where
     where
         Q: Hash + Equivalent<K> + ?Sized,
     {
-        if !self.table.raw.is_empty() {
+        if !self.table.is_empty() {
             let hash = make_hash::<Q, S>(&self.hash_builder, k);
-            self.table.raw.get(hash, equivalent_key(k)).is_some()
+            self.table.find(hash, equivalent_key(k)).is_some()
         } else {
             false
         }
@@ -1409,9 +1402,9 @@ where
         Q: Hash + Equivalent<K> + ?Sized,
     {
         // Avoid `Option::map` because it bloats LLVM IR.
-        if !self.table.raw.is_empty() {
+        if !self.table.is_empty() {
             let hash = make_hash::<Q, S>(&self.hash_builder, k);
-            match self.table.raw.get_mut(hash, equivalent_key(k)) {
+            match self.table.find_mut(hash, equivalent_key(k)) {
                 Some(&mut (_, ref mut v)) => Some(v),
                 None => None,
             }
@@ -1998,7 +1991,7 @@ where
     /// primarily used for memory profiling.
     #[inline]
     pub fn allocation_size(&self) -> usize {
-        self.table.raw.allocation_size()
+        self.table.allocation_size()
     }
 }
 
